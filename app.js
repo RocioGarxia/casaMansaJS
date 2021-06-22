045962bb3075c3495728b8dd1d419c90afc0d845
RocioGarxia <rociogarciacrespo@gmail.com>
2021-06-22 00:27:50 -0300

    cambiando las cards

--- app.js
+++ app.js
@@ -1,60 +1,72 @@
-/*
+
 let carritoDeCompras = []
-const contenedorProducto = document.getElementById('contenedor-productos')
+
+const contenedorProductos = document.getElementById('contenedor-productos')
 const contenedorCarrito = document.getElementById('carrito-contenedor')
 
 const contadorCarrito = document.getElementById('contadorCarrito')
 const precioTotal = document.getElementById('precioTotal')
 
+const selectTamaño = document.getElementById('selectTamaño')
+
+selectTamaño.addEventListener('change', ()=>{
+    console.log(selecTalles.value)
+
+    if (selectTamaño.value == "all") {
+        mostrarProductos(stockProductos)
+    } else {
+        mostrarProductos(stockProductos.filter((el)=> el.talle == selectTamaño.value))
+    }
+
+})
+
+
 mostrarProductos(stockProductos)
 
-function mostrarProductos(array) {
-   // array.forEach((x) => { //
-   contenedorProductos.innerHTML = ''
+function mostrarProductos(array){
+    contenedorProductos.innerHTML = ''
     array.forEach((productoNuevo)=>{
         let div = document.createElement('div')
         div.classList.add('producto')
         div.innerHTML += `
-            <img src= ${x.img} alt="laminas">
-            <h2> ${x.lamina}</h2>
-            <p> Autor: ${x.autor}</p>
-            <p> Técnica: ${x.tecnica}</p>
-            <p> Tamaño: ${x.tamaño}</p>
-            <p> Número de Serie: ${x.numSerie}</p>
-            <p class= "precioProducto"> Precio: $ ${x.precio}</p>
-            <button id= ${x.id} class = "boton-agregar"> Agregar <i class = "fas fa-shopping-cart"> </i> </button>
+            <img src= ${productoNuevo.img} alt="laminas">
+            <h2> ${productoNuevo.lamina}</h2>
+            <p> Autor: ${productoNuevo.autor}</p>
+            <p> Técnica: ${productoNuevo.tecnica}</p>
+            <p> Tamaño: ${productoNuevo.tamaño}</p>
+            <p> Número de Serie: ${productoNuevo.numSerie}</p>
+            <p class= "precioProducto"> Precio: $ ${productoNuevo.precio}</p>
+            <button id="boton${productoNuevo.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
                         `
 
-        contenedorProducto.appendChild(div)
+                        contenedorProductos.appendChild(div)
 
        // let boton = document.getElementById(`${x.id}`) //
 
        let boton = document.getElementById(`boton${productoNuevo.id}`)
       //  boton.addEventListener('click', () => { agregarAlCarrito(`${x.id}`) }) //
 
-        boton.addEventListener('click', ()=>{
-            agregarAlCarrito(productoNuevo.id)
-         })
-    })
+      boton.addEventListener('click', ()=>{
+        agregarAlCarrito(productoNuevo.id)
+     })
+})
 }
 
-// no pude hacer que el contador de un mismo ítem lo cuente como dos con length++, si bien entiendo que esta parte del código es correcta //
+
 
 function agregarAlCarrito(id) {
-    let validar = carritoDeCompras.some(x => x.id == id)
-    console.log(validar)
     
-    if (validar) {
+    let validar = carritoDeCompras.some(x=> x.id == id)
+    console.log(validar)
+    if(validar){
         console.log(carritoDeCompras)
         let count = document.getElementById(`cantidad${id}`)
-        // contadorCarrito.innerText = carritoDeCompras.length++ //
+        
        
-        //linea nueva de codigo//
-
         carritoDeCompras.map(x=>{
-            if(x.id == id){
-            count.innerText = `cantidad: ${x.cantidad += 1}`
-            actualizarCarrito()
+        if(x.id == id){
+        count.innerText = `cantidad: ${x.cantidad += 1}`
+        actualizarCarrito()
         }
     })
         
@@ -70,11 +82,11 @@ function agregarAlCarrito(id) {
         let div = document.createElement('div')
         div.classList.add('productoEnCarrito')
         div.innerHTML = `
-                <p>${productoAgregar.lamina}</p>
-                <p>Precio: $ ${productoAgregar.precio}</p>
-                <button id= "eliminar ${productoAgregar.id}" class = "boton-eliminar"> <i class = "fas fa-trash-alt"> </i> </button>
-                     `
-
+        <p>${productoAgregar.nombre}</p>
+        <p id=cantidad${productoAgregar.id}>cantidad:${productoAgregar.cantidad}</p>
+        <p>Precio: $${productoAgregar.precio}</p>
+        <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
         contenedorCarrito.appendChild(div)
 
         //nuevas lineas//
@@ -86,39 +98,37 @@ function agregarAlCarrito(id) {
             actualizarCarrito()
             }
         })
+    let botonEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
 
-        let botonEliminar = document.getElementById(`eliminar ${productoAgregar.id}`)
-        botonEliminar.addEventListener('click', () => {
-            botonEliminar.parentElement.remove()
-            console.log(botonEliminar.parentElement)
-            carritoDeCompras = carritoDeCompras.filter((el) => el.id != productoAgregar.id)
-            console.log(carritoDeCompras)
-            actualizarCarrito()
-
-        })
+    botonEliminar.addEventListener('click', ()=>{
+        botonEliminar.parentElement.remove()
+        console.log(botonEliminar.parentElement)
+        carritoDeCompras = carritoDeCompras.filter((el) => el.id != productoAgregar.id) 
+        console.log(carritoDeCompras)    
+        actualizarCarrito()
+    })
     }
 }
 
-    function actualizarCarrito() {
-      //  contadorCarrito.innerText = carritoDeCompras.length //
+function actualizarCarrito() {
+    
+    contadorCarrito.innerText = carritoDeCompras.reduce((acc, el) => acc + el.cantidad, 0)
+    
+    localStorage.setItem('carritoDeCompras', JSON.stringify(carritoDeCompras))
+    precioTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0)
+}
 
-      //nueva linea//
-      contadorCarrito.innerText = carritoDeCompras.reduce((acc, el) => acc + el.cantidad, 0)
 
-        localStorage.setItem('carritoDeCompras', JSON.stringify(carritoDeCompras))
-        precioTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0)
+function revisarLocal (){
+    let carritoLocal = JSON.parse(localStorage.getItem('carritoDeCompras'))
+    if(carritoLocal){
+        carritoLocal.forEach((el)=>agregarAlCarrito(el.id))
     }
+}
 
-   function revisarLocal () {
-        let carritoLocal = JSON.parse(localStorage.getItem('carritoDeCompras'))
-        if (carritoLocal){
-            carritoLocal.forEach((el)=> agregarAlCarrito(el.id))
-        }
-    }
+revisarLocal()
 
-    revisarLocal () 
 
-*/
 
 $(document).ready(function(){
 
